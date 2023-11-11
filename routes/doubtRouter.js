1f2b85d0689817b6133437ac7002da6f5b975368
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-11-11 10:53:58 +0530

    doubt is in progress

--- routes/doubtRouter.js
+++ routes/doubtRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { authMiddleware } = require("../middlewares/authMiddleware");
+const DoubtModel = require("../models/doubtModel");
 const doubtRouter = express.Router();
 
 doubtRouter.get('/history',authMiddleware,async(req,res)=>{
-    
+    const userId = req.userId
+    const doubts = await DoubtModel.find({userId})
 })
 
 module.exports = {doubtRouter};
