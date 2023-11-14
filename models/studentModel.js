1780ea127e7fca536ca3a1e17d8d74017aacb49b
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-11-14 11:04:01 +0530

    model updated

--- models/studentModel.js
+++ models/studentModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
     userId : {type:mongoose.Schema.Types.ObjectId, ref : 'user'},
+    name : {type:String},
     classGrade : {type:String,required:true},
     language : {type: String,required:true},
 })
--- models/tutorModel.js
+++ models/tutorModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const tutorSchema = new mongoose.Schema({
     userId : {type:mongoose.Schema.Types.ObjectId, ref : 'user'},
+    name : {type:String},
     classGrade : {type:String,required:true},
     language : {type: String,required:true},
     subjectExpertise : {type:String,required:true}
